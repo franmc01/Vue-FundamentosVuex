ab1f81937120f6ae45cac0e0d0414d39da65bc11
franciscomarin98 <francmarinc@gmail.com>
2021-06-27 16:31:41 -0500

    Entendiendo la lógica de vuex

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ProductList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductList from './pages/ProductList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductList
   }
 }
 </script>
---
+++ src/pages/ProductList.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div>
+    <h1>Listado de productos</h1>
+    <hr />
+    <ul>
+      <li v-for="producto in productos" :key="producto.id">
+        {{ producto.title }} | {{ producto.price }}
+        <i>{{ producto.inventory }}</i>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import store from "@/store/index.js";
+export default {
+  name: "ProductList",
+  computed: {
+    productos() {
+      return store.state.productos;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
