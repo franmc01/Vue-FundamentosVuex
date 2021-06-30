1ebc6ebdf01eab0a52a9d28eaecd1a72ebe4ecc9
franciscomarin98 <francmarinc@gmail.com>
2021-06-30 15:00:51 -0500

    Introducciendome a VUEX

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <AppProductList />
 </template>
+
+<script>
+import AppProductList from "./components/AppProductList";
+export  default  {
+  name: 'App',
+  components: {
+    AppProductList
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/AppProductList.vue
@@ -0,0 +1,25 @@
+<template>
+<h1>Tienda Sisco</h1>
+  <hr />
+  <ul>
+    <li v-for="item in productos" :key="item.id">
+      {{ item.title }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import store from "../store/index";
+export default {
+  name: "AppProductList",
+  computed: {
+    productos() {
+      return store.state.productos;
+}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
