e03229599a93ff7e4e262bc9c509960bfc10688b
franciscomarin98 <francmarinc@gmail.com>
2021-07-02 11:01:17 -0500

    Creando la funcionalidad el producto

--- src/components/AppProductList.vue
+++ src/components/AppProductList.vue
@@ -5,7 +5,8 @@
     <ul>
       <li v-for="product in productos" :key="product.id">
         {{ product.title }} | $ {{ product.price }} |
-        <i>{{ product.inventory }} unidades </i>
+        <i>{{ product.inventory }} &nbsp;</i>
+        <button @click="addToCart(product)">Cart</button>
       </li>
     </ul>
   </div>
@@ -16,19 +17,24 @@ import store from "../store/index";
 
 export default {
   name: "AppProductList",
+  async created() {
+    try {
+      await store.dispatch("getProducts");
+    } catch (error) {
+      console.error(error);
+    }
+  },
   computed: {
     productos() {
       //return store.state.productos;
       return store.getters.productsOnStock;
     }
   },
-  created() {
-    try {
-      store.dispatch('getProducts');
-    } catch (e) {
-      console.log(e)
+  methods: {
+    addToCart(product) {
+      store.dispatch('addProductToCart', product);
     }
-  }
+  },
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,60 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 import api from "../api/shop";
 
 export default createStore({
-  state: {
-    productos: []
-  },
-  mutations: {
-    setProductos(state, productos) {
-      state.productos = productos;
-    }
-  },
-  actions: {
-    getProducts(context){
-      return new Promise((resolve, reject) => {
-        api.getProducts(productos => {
-          context.commit('setProductos', productos);
-          resolve();
-        });
-      })
-    }
-  },
-  getters: {
-    productsOnStock(state){
-      return state.productos.filter(producto => (producto.inventory > 0));
-    }
-  },
-  modules: {
-  }
+    state: {
+        productos: [],
+        carrito: []
+    },
+    mutations: {
+        setProductos(state, productos) {
+            state.productos = productos;
+        },
+        incrementProductQuantity(state, existe) {
+            existe.quantity++;
+        },
+        addToCart(state, producto) {
+            state.carrito.push({
+                id: producto.id,
+                quantity: producto.quantity
+            });
+        },
+        decrementProductInventory(state, producto) {
+            producto.inventory--;
+        }
+    },
+    actions: {
+        getProducts(context) {
+            return new Promise((resolve) => {
+                api.getProducts(productos => {
+                    context.commit('setProductos', productos);
+                    resolve();
+                });
+            })
+        },
+        addProductToCart(context, producto) {
+            //Verificar si hay inventario
+            if (producto.inventory === 0) return;
+            //Verificar si existe el producto en el carrito
+            const existe = context.state.carrito.find(item => item.id === producto.id);
+            if (existe) {
+                //Si es así, añadir uno más a la compra
+                context.commit('incrementProductQuantity', existe);
+            } else {
+                //Caso contrario, el producto al carrito
+                context.commit('addToCart', producto);
+            }
+            //Independientemente, restar el inventario del producto
+            context.commit('decrementProductInventory', producto);
+        }
+
+    },
+    getters: {
+        productsOnStock(state) {
+            return state.productos.filter(product => {
+                return product.inventory > 0;
+            });
+        },
+    },
+    modules: {}
 })
